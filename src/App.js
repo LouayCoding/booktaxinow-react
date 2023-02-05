bca361978e66f684af60a48e09ddf14efb5eca05
Louay Attahiri <louay-attahiri@hotmail.com>
2023-02-05 20:26:35 +0100

    Navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Books from './Books';
 import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/books" element={<Books/>}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/books" element={<Books />}></Route>
+      </Routes>
+    </>
+
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,54 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <section data-bs-version="5.1" className="menu popup-btn-cards menu03 photom4_menu03 cid-ttAkBSb5oX" once="menu" id="menu03-10">
+            <nav className="navbar navbar-dropdown navbar-expand-lg">
+                <div className="mbr-overlay" style={{ opacity: 1 }} />
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="left-menu">
+                        <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                            <CustomLink to="/">HOME</CustomLink>
+                            <CustomLink to="/about">ABOUT</CustomLink>
+                            <CustomLink to="/tarieven">TARIEVEN</CustomLink>
+                        </ul>
+                    </div>
+                    <div className="brand-container">
+                        <div className="navbar-brand">
+                            <span className="navbar-caption-wrap"><a className="navbar-caption text-primary display-5" href="#">BOOKTAXINOW</a></span>
+                        </div>
+                    </div>
+                    <div className="right-menu">
+                        <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                            <CustomLink to="/prices">PRICES</CustomLink>
+                            <CustomLink to="/book">BOOK</CustomLink>
+                            <CustomLink to="/contact">CONTACT</CustomLink>
+                        </ul>
+                    </div>
+                </div>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <div className="hamburger">
+                        <span />
+                        <span />
+                        <span />
+                        <span />
+                    </div>
+                </button>
+            </nav>
+        </section>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={"nav-item"}>
+            <Link className={isActive ? 'nav-link link text-primary display-4' : 'nav-link link text-white display-4'} to={to} {...props}>
+                {children}
+            </Link>
+
+        </li>
+    )
+}
